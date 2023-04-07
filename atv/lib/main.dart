6d34f6e4a29e07c0f6c7f52986d9328b77952916
DayXL <day.luc.xav0606@gmail.com>
2023-04-07 16:36:33 -0300

    separando os botões

--- atv/lib/main.dart
+++ atv/lib/main.dart
@@ -35,7 +35,6 @@ class SigHat extends StatefulWidget {
 }
 
 class _SigHatState extends State<SigHat> {
-
   @override
   Widget build(BuildContext context) {
     const int tabsCount = 2;
@@ -89,119 +88,125 @@ class _SigHatState extends State<SigHat> {
               stops: const [0.3, 1],
             ),
           ),
-          child: TabBarView(
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Flexible(
-                    child: TextButton.icon(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.grey[400],
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 26, vertical: 13),
-                        ),
-                        label: const Text('Modúlo Boné'),
-                        icon: const Icon(Icons.whatshot),
-                        onPressed: () {
-                          Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ModulBon()));
 
-                        }),
-                  ),
+          child: const MeusBotoes(),
 
-                  Flexible(
-                    child: TextButton.icon(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.grey[400],
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 26, vertical: 13),
-                        ),
-                        label: const Text('Modúlo Fornecedor'),
-                        icon: const Icon(Icons.airport_shuttle),
-                        onPressed: () {}),
-                  ),
-                  Flexible(
-                    child: TextButton.icon(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.grey[400],
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 26, vertical: 13),
-                        ),
-                        label: const Text('Modúlo Estoque'),
-                        icon: const Icon(Icons.all_inclusive),
-                        onPressed: () {
-                          
-
-                        }),
-                  ),
-                  Flexible(
-                    child: TextButton.icon(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.grey[400],
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 26, vertical: 13),
-                        ),
-                        label: const Text('Relatórios'),
-                        icon: const Icon(Icons.add_chart),
-                        onPressed: () {
-                          Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ModulRlt()));
-                          
-                        }),
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Flexible(
-                    child: TextButton.icon(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.grey[400],
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 26, vertical: 13),
-                        ),
-                        label: const Text('Modúlo Cliente'),
-                        icon: const Icon(Icons.people_alt),
-                        onPressed: () {
-                          Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ModulClt()));
-
-                        }),
-                  ),
-                  Flexible(
-                    child: TextButton.icon(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.grey[400],
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 26, vertical: 13),
-                        ),
-                        label: const Text('Modúlo Pedido'),
-                        icon: const Icon(Icons.add_shopping_cart),
-                        onPressed: () {}),
-                  ),
-                  Flexible(
-                    child: TextButton.icon(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.grey[400],
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 26, vertical: 13),
-                        ),
-                        label: const Text('Fechar programa'),
-                        icon: const Icon(Icons.adjust),
-                        onPressed: () {}),
-                  ),
-                ],
-              ),
-            ],
-          ),
         ),
       ),
     );
   }
+}
 
+class MeusBotoes extends StatelessWidget {
+  const MeusBotoes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Flexible(
+              child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[400],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 26, vertical: 13),
+                  ),
+                  label: const Text('Modúlo Boné'),
+                  icon: const Icon(Icons.whatshot),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ModulBon()));
+                  }),
+            ),
+            Flexible(
+              child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[400],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 26, vertical: 13),
+                  ),
+                  label: const Text('Modúlo Fornecedor'),
+                  icon: const Icon(Icons.airport_shuttle),
+                  onPressed: () {}),
+            ),
+            Flexible(
+              child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[400],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 26, vertical: 13),
+                  ),
+                  label: const Text('Modúlo Estoque'),
+                  icon: const Icon(Icons.all_inclusive),
+                  onPressed: () {}),
+            ),
+            Flexible(
+              child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[400],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 26, vertical: 13),
+                  ),
+                  label: const Text('Relatórios'),
+                  icon: const Icon(Icons.add_chart),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ModulRlt()));
+                  }),
+            ),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Flexible(
+              child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[400],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 26, vertical: 13),
+                  ),
+                  label: const Text('Modúlo Cliente'),
+                  icon: const Icon(Icons.people_alt),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ModulClt()));
+                  }),
+            ),
+            Flexible(
+              child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[400],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 26, vertical: 13),
+                  ),
+                  label: const Text('Modúlo Pedido'),
+                  icon: const Icon(Icons.add_shopping_cart),
+                  onPressed: () {}),
+            ),
+            Flexible(
+              child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[400],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 26, vertical: 13),
+                  ),
+                  label: const Text('Modúlo OS'),
+                  icon: const Icon(Icons.apartment),
+                  onPressed: () {}),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
