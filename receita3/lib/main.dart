b27d8f9c3195f6c0c427017f90fdf6c8fb4fa6c3
DayXL <day.luc.xav0606@gmail.com>
2023-04-16 16:57:28 -0300

    criar vários botões PopUp

--- receita3/lib/main.dart
+++ receita3/lib/main.dart
@@ -1,112 +1,78 @@
 import 'package:flutter/material.dart';
 
+List<IconData> icones = [Icons.search, Icons.upload, Icons.exit_to_app];
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-
-      debugShowCheckedModeBanner: false,
-
-      home: Scaffold(
-        
-        appBar: const PreferredSize(
-          preferredSize: Size.fromHeight(kToolbarHeight),
-          child: MyAppBar(),
-        ),
-        
-        body: DataBodyWidget(objects:const [
-
-          "La Fin Du Monde - Bock - 65 ibu",
-
-          "Sapporo Premiume - Sour Ale - 54 ibu",
-
-          "Duvel - Pilsner - 82 ibu"
-
-        ]),
-
-        bottomNavigationBar: NewNavBar(icones:const [
-          Icons.home,
-          Icons.search,
-          Icons.favorite,
-          Icons.settings,
-          Icons.abc,
-          Icons.e_mobiledata,
-
-        ]),
-
-    ));
-
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: const PreferredSize(
+            preferredSize: Size.fromHeight(kToolbarHeight),
+            child: MyAppBar(),
+          ),
+          body: DataBodyWidget(objects: const [
+            "La Fin Du Monde - Bock - 65 ibu",
+            "Sapporo Premiume - Sour Ale - 54 ibu",
+            "Duvel - Pilsner - 82 ibu"
+          ]),
+          bottomNavigationBar: NewNavBar(icones: const [
+            Icons.home,
+            Icons.search,
+            Icons.favorite,
+            Icons.settings,
+            Icons.abc,
+            Icons.e_mobiledata,
+          ]),
+        ));
   }
-
 }
 
 class NewNavBar extends StatelessWidget {
-
   List<IconData> icones;
 
-  NewNavBar({super.key, this.icones = const [] });
+  NewNavBar({super.key, this.icones = const []});
 
   @override
-
   Widget build(BuildContext context) {
-
-    return Row(children: icones.map( 
-
-      (obj) => Expanded(
-
-        child: IconButton(
-            icon: Icon(obj),
-            onPressed: () {},
-          ),
-
-        )
-
-      ).toList());
-
+    return Row(
+        children: icones
+            .map((obj) => Expanded(
+                  child: IconButton(
+                    icon: Icon(obj),
+                    onPressed: () {},
+                  ),
+                ))
+            .toList());
   }
-
 }
 
 class DataBodyWidget extends StatelessWidget {
+  List<String> objects;
 
- List<String> objects;
+  DataBodyWidget({this.objects = const []});
 
-  DataBodyWidget( {this.objects = const [] });
-
-  Expanded processarUmElemento(String obj){
-
-    return Expanded(                
-
-          child: Center(child: Text(obj)),
-
-        );
-
+  Expanded processarUmElemento(String obj) {
+    return Expanded(
+      child: Center(child: Text(obj)),
+    );
   }
 
   @override
-
   Widget build(BuildContext context) {
-
-    return Column(children: objects.map( 
-
-      (obj) => Expanded(
-
-        child: Center(child: Text(obj)),
-
-        )
-
-      ).toList());
-
+    return Column(
+        children: objects
+            .map((obj) => Expanded(
+                  child: Center(child: Text(obj)),
+                ))
+            .toList());
   }
-
 }
 
 class MyAppBar extends StatelessWidget {
@@ -120,29 +86,24 @@ class MyAppBar extends StatelessWidget {
         PopupMenuButton(
           icon: const Icon(Icons.more_vert),
           itemBuilder: (context) {
-            return [
-              const PopupMenuItem(
-                child: Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
-              ),
-              const PopupMenuItem(
-                child: Icon(
-                  Icons.upload,
-                  color: Colors.black,
-                ),
-              ),
-              const PopupMenuItem(
-                child: Icon(
-                  Icons.exit_to_app,
-                  color: Colors.black,
-                ),
-              ),
-            ];
+            return criarPopupItens(icones);
           },
+          color: const Color.fromARGB(255, 194, 167, 240),
         )
       ],
     );
+  }
+
+  List<PopupMenuItem<IconData>> criarPopupItens(List<IconData> icones) {
+    return icones
+        .map((icone) => PopupMenuItem(
+              value: icone,
+              child: Row(
+                children: [
+                  Icon(icone, color: Colors.black),
+                ],
+              ),
+            ))
+        .toList();
   }
 }
